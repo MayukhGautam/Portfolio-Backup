7c7a5787a11e1179c6ed667ae63495ea36ccd2e8
MayukhGautam <54997204+MayukhGautam@users.noreply.github.com>
2020-01-25 22:56:35 -0500

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,1 +1,12 @@
+function setup() {
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.parent('can');
+}
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+}
