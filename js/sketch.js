e108b03d121a0cf91d1cdbd75cf9f80ec3184193
MayukhGautam <54997204+MayukhGautam@users.noreply.github.com>
2020-01-28 21:09:45 -0500

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -7,36 +7,55 @@ function preload(){
 function setup() {
   rectMode(CENTER);
   textAlign(CENTER);
-  let cnv = createCanvas(windowWidth, 10000);
+  let cnv = createCanvas(windowWidth, 11000);
   cnv.parent('can');
+
+  nsVid = createVideo(['/js/vid/Ns.mp4'], vidLoad);
+  nsVid.size(700,700);
+  nsVid.position(200,3200);
+}
+
+function vidLoad() {
+  nsVid.loop();
+  nsVid.volume(0);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, 10000);
+
+  if(windowWidth >= 1500){
+    resizeCanvas(windowWidth, 10000);
+  }else{
+    resizeCanvas(1500, 10000);
+  }
+
 }
 
 function draw() {
-  let list = ["Intro to Js", "Intro to Js", "Intro to HTML/CSS", "Advanced JS:Natural Simulations", "Algorithms", "Coding Challenge", "MicroBit", "Intro to SQL", "Computers", "The Internet"];
+  let list = ["Intro to Js", "Algorithms", "Intro to HTML/CSS", "Natural Simulations", "Algorithms", "Coding Challenge", "MicroBit", "Intro to SQL", "Computers", "The Internet"];
   let bg = color(0,202,202,155);
   background(255);
   textFont(titleF, 35);
 
   fill(bg);
   noStroke();
-  rect(windowWidth/2,50,windowWidth,100,5);
+  rectMode(CENTER);
+  rect(width/2,50,width,100,5);
 
   fill(255);
-  text("Mayukh's Portfolio", windowWidth/2, 60);
+  textAlign(CENTER);
+  text("Mayukh's Portfolio", width/2, 60);
 
   //Main rect
   fill(bg);
-  rect(windowWidth/2, 5200, windowWidth, 10000,5);
+  rectMode(CORNER);
+  rect(0, 200, width, 11000,5);
 
   fill(255);
   textSize(100);
+  textAlign(CORNER);
   for(var i = 0; i<10; i++){
 
-    text(i+1 + ")", windowWidth-(windowWidth-70), 300 + i*1000);
+    text(i+1 + ") " + list[i], width-(width-40), 300 + i*1000);
 
   }
 
