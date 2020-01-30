8c377cfdff9acb5b0624a889f8a5a644bb9ca19c
MayukhGautam <54997204+MayukhGautam@users.noreply.github.com>
2020-01-29 19:37:15 -0500

    Update A_Star.js

--- js/A_Star.js
+++ js/A_Star.js
@@ -1,397 +1,77 @@
-/*
-      MAYUKH MIKE YASIR
 
-      Version 3.1
+let xPositions = [500];
+let yPositions = [655];
 
-      ADDED NON OBJECT ORIENTED OBSTACLES
-
-*/
-
-function removeFromArray(arr, elt){
-
-  for (var i = arr.length-1; i >= 0 ; i--){
-
-    if(arr[i] == elt){
-
-      arr.splice(i,1);
-
-    }
+mouseClicked = function () {
+    if(mouseX >= 197 && mouseX <= 587  && mouseY <= 1050 && mouseY >= 655){
+    xPositions.push(mouseX);
+    yPositions.push(655);
   }
 }
 
-function heuristic(a,b){
+function preload(){
+  qFont = loadFont('/js/Quicksand-Medium.ttf');
+  titleF = loadFont("/js/Quicksand-Bold.ttf");
+}
 
-  return ( abs( a.i - b.i ) + abs( a.j - b.j ) );
+function setup() {
+  let cnv = createCanvas(windowWidth, 11000);
+  cnv.parent('can');
+
+  A_Setup(); //A* embedded
+  let start_A = createButton('Start / Stop');
+  start_A.position(720,5330);
+  start_A.mousePressed(flip);
+
+  let start_R = createButton('Click ON/OFF');
+  start_R.position(600,650);
+  start_R.mousePressed(beg);
+}
+
+function windowResized() {
+
+  if(windowWidth >= 1500){
+    resizeCanvas(windowWidth, 10000);
+  }else{
+    resizeCanvas(1500, 10000);
+  }
 
 }
 
-function blink(num){
+function draw() {
+  let list = ["Intro to Js", "Games & Visualizations", "Intro to HTML/CSS", "Natural Simulations", "Algorithms", "A* Pathfinder", "MicroBit", "Intro to SQL", "Computers", "The Internet"];
+  let bg = color(0,202,202,155);
+  background(255);
+  textFont(titleF, 35);
 
-  if(num%25 <= 12){
+  fill(bg);
+  noStroke();
+  rectMode(CENTER);
+  rect(width/2,50,width,100,5);
 
-   return(color(188,0,188));
+  fill(255);
+  textAlign(CENTER);
+  text("Mayukh's Portfolio", width/2, 60);
 
-  }else{ return(color(255,255,0)); }
+  //Main rect
+  fill(bg);
+  rectMode(CORNER);
+  rect(0, 200, width, 11000,5);
 
-}
+  fill(255);
+  textSize(100);
+  textAlign(LEFT);
+  for(var i = 0; i<10; i++){
 
-const squares = 30;
-let openSet = [];
-let closedSet = [];
-let SBlock = [];
-let start;
-let end;
-let current;
-let w, h;
-let count = 0;
-let done = false;
-let path = [];
-let clicked = false;
-
-var grid = new Array(squares);
-
-function flip(){
-  if(clicked === false){clicked = true;}
-  else {clicked = false;}
-}
-
-class Spot{
-
-  constructor(i,j){
-    this.i = i;
-    this.j = j;
-    this.f = 0;
-    this.g = 0;
-    this.h = 0;
-    this.points = 1;
-    this.donated = false;
-    this.pass = true;
-    this.previous = undefined;
-    this.neighbors = [];
-  }
-
-
-}
-
-  Spot.prototype.show = function(col){
-
-    fill(col);
-    noStroke();
-    rect((this.i*w)+150, (this.j*h)+5330, w-1, h-1 );
+    text(i+1 + ") " + list[i], width-(width-40), 300 + i*1000);
 
   }
 
+  fill(0);
+  rect(140,5320, 570, 570, 5);
+  rain(xPositions, yPositions);
+  Pathfind(); //a* code
 
-  Spot.prototype.addNeighbors = function(grid){
 
-    let i = this.i;
-    let j = this.j;
-
-    if (i < squares - 1){
-
-      this.neighbors.push(grid[i + 1][j]);
-
-    }
-
-    if (i > 0){
-
-      this.neighbors.push(grid[i - 1][j]);
-
-    }
-
-    if(j < squares-1){
-
-      this.neighbors.push(grid[i][j + 1]);
-
-    }
-
-    if(j > 0){
-
-      this.neighbors.push(grid[i][j - 1]);
-
-    }
-
-  }
-
-class Super extends Spot{
-
-  super(){}
-
-  wall(){
-
-    this.f = 10000;
-    this.pass = false;
-    this.points = 0;
-
-  }
-
-  pWall(){
-
-    this.points = -4;
-
-  }
-
-  donate(list){
-
-    var atleastone = false
-
-    for(var i = 0; i<list.length; i++){
-
-      for(var j = 0; j<this.neighbors.length; j++){
-
-        if(list[i] === this.neighbors[j]){
-
-          list[i].points = this.points;
-          atleastone = true;
-
-        }else{ this.donated = false; }
-      }
-    }
-
-    if (atleastone === true){
-
-      this.donated = true;
-
-    }
-
-
-  }
-}
-
-
-function A_Setup() {
-
-  w = 550 / squares;
-  h = 550/ squares;
-
-  //Making a 2d Array
-  for (let i = 0; i < squares; i++){
-
-    grid[i] = new Array(squares);
-
-  }
-
-  for (let i = 0; i < squares; i++){
-
-     for (let j = 0; j < squares; j++){
-
-      var check = ((i === 0 && j === 0) || (i === 1 && j === 1) || (i === 0 && j === 1) || (i === 1 && j === 0));
-
-      var check2 = ((i === squares-1 && j === squares-1) || (i === squares-2 && j === squares-2) || (i === squares-2 && j === squares-1) || (i === squares-1 && j === squares-2));
-
-      if((floor(random(0,100)) <= 25) && check === false && check2 === false){
-
-        grid[i][j] = new Super(i,j)
-        grid[i][j].wall();
-
-        if((floor(random(0,100)) <= 10)){
-
-          grid[i][j].pWall();
-
-        }
-
-        SBlock.push(grid[i][j]);
-
-      }
-      else{ grid[i][j] = new Spot(i,j); }
-
-    }
-
-  }
-
-  for ( i = 0; i < squares; i++){
-
-    for (let j = 0; j < squares; j++){
-
-      grid[i][j].addNeighbors(grid);
-
-    }
-
-  }
-
-  start = grid[0][0];
-  end = grid[squares - 1][squares - 1];
-
-  start.pass = true;
-  end.pass = true;
-  start.f = 0;
-  end.f = 0;
-
-  openSet.push(start);
-}
-
-function Pathfind() {
-  if(clicked === true){
-
-    if(openSet.length > 0){
-
-      let winner = 0;
-      for(var i = 0; i < openSet.length; i++){
-
-        if(openSet[i].f < openSet[winner].f){
-
-          winner = i;
-
-        }
-
-      }
-
-      current = openSet[winner];
-
-      if(openSet[winner] === end){
-
-        done = true;
-        noLoop();
-
-
-      }
-
-
-      removeFromArray(openSet,current);
-      closedSet.push(current);
-
-      let neighbors = current.neighbors;
-
-      for( i = 0; i<neighbors.length; i++){
-
-        var neighbor = neighbors[i];
-
-        if (!closedSet.includes(neighbor)){
-
-          let tempG = current.g + 1;
-
-
-          if(openSet.includes(neighbor)){
-
-            if (tempG < neighbor.g){
-
-              neighbor.g = tempG;
-
-            }
-
-          } else {
-
-            neighbor.g = tempG;
-            openSet.push(neighbor);
-
-          }
-
-          if(neighbor.pass === true)
-          {
-
-          neighbor.h = heuristic(neighbor, end);
-          neighbor.f = neighbor.g + neighbor.h;
-          neighbor.previous = current;
-
-          }
-
-        }
-
-      }
-
-      // Find the path
-    path = [];
-    let temp = current;
-    path.push(temp);
-    while(temp.previous){
-
-      path.push(temp.previous);
-      temp = temp.previous;
-
-    }
-
-      //we can keep going
-    }else{
-      //no solution
-
-    }
- }
-
-  for (let i = 0; i < squares; i++){
-
-    for(var j = 0; j < squares; j++){
-
-        grid[i][j].show(color(255));
-
-    }
-
-  }
-
-
-  for ( let i = 0; i < closedSet.length; i++){
-
-      closedSet[i].show(color(255,0,0));
-
-  }
-
-   for ( let i = 0; i < openSet.length; i++){
-
-      openSet[i].show(color(0,255,0));
-
-  }
-
-  for(let i = 0; i<SBlock.length; i++) {
-
-    if(SBlock[i].points === -4){
-
-      SBlock[i].donate(path);
-
-      if(SBlock[i].donated === false){
-
-        SBlock[i].show(color(188,0,188));
-
-      }else{
-
-        SBlock[i].show(blink(count));
-      }
-
-
-    }else{
-
-      SBlock[i].show(color(0));
-
-    }
-
-  }
-
-
-  for ( let i = 0; i< path.length; i++) {
-
-    path[i].show(color(0,255,255));
-
-  }
-
-  if(done === true){
-
-    let tPoints = 0;
-
-    for( let i = 0; i<path.length; i++){
-
-       tPoints += path[i].points
-
-    }
-
-    for( let i = 0 ; i<SBlock.length; i++){
-
-      if(SBlock[i].donated === true){
-
-          SBlock[i].show(color(255,255,0));
-
-      }
-    }
-
-    fill(255);
-    rect(580,5330,120,100);
-    fill(0);
-
-    textSize(20);
-    text(tPoints + " Steps!", 580,5355);//explain points
-
-    text("Yellow : -5", 580,5380);
-
-    text("Blue : +1", 580,5405);
-  }
-
-  count++;
 
 }
